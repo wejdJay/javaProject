940ef3f85d6da5748bbd7f27429e952d92f8a60d
wejdJay <129787480+wejdJay@users.noreply.github.com>
2024-02-17 00:21:21 +0300

    Add files via upload

---
+++ javaProject/Mainn.java
@@ -0,0 +1,17 @@
+package com.mycompany.mainn;
+
+
+public class Mainn{
+    public static void main (String[]args){
+        
+        String [] StudentsNames = {"Wejdan" , "Khalid" , "Ahmad"};
+        System.out.println(StudentsNames);
+        
+        StudentsNames[0]="Khalid";
+        StudentsNames[1]="Wejdan";
+        StudentsNames[2]="Ahmad";
+        System.out.println(StudentsNames);
+    }
+    
+    
+}
